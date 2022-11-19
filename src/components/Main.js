b0238e487363e5d847c5c096490359fbcf39db87
Emre Yılmazer <submorarts@gmail.com>
2022-11-19 19:48:20 +0300

    v2

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,37 +2,55 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import { Container, Row, Col } from 'reactstrap'
 import { useState } from 'react';
-import axios from 'axios';
+import { useEffect } from 'react';
 
 function Main() {
 
     const weatherCities = useSelector((state) => state.weatherList.mainArr);
-    const weatherCitiesFilter = useSelector((state) => state.weatherList.filterArr);
+    const weatherCitiesFilter = weatherCities
+
+
+    const [weathers, setWeathers] = useState(null)
 
 
 
-    const [city, setCity] = useState(weatherCitiesFilter)
 
-    const getWeatherData = async (lat, lon) => {
 
-        const key = process.env.REACT_APP_API_KEY
 
-        try {
 
-            const { data } = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${key}`)
 
-            setCity(data);
-        }
+    const handleChange = (index) => {
 
-        catch {
+        const currentCity = weatherCities[index]
 
-            alert("Veri alınırken hata oluştu.")
-        }
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${currentCity.latitude}&lon=${currentCity.longitude}0&appid=${process.env.REACT_APP_API_KEY}&exclude=minutely,hourly&units=metric`)
+            .then(response => response.json())
+            .then(data => setWeathers(data))
+            .catch(err => console.log("error"))
 
 
     }
 
-    console.log(city.name);
+
+
+    useEffect(() => {
+
+
+
+        if (weathers) {
+
+            const dateVarMon = weathers.daily[0].dt
+            const dateMon = new Date(dateVarMon * 1000)
+
+
+            console.log(weathers.daily)
+        }
+
+
+
+    }, [weathers])
+
 
 
     return (
@@ -43,23 +61,27 @@ function Main() {
 
                     <Col xs="12">
                         <h3 className='my-3'>
-                            Türkiye Haftalık Hava Durumu
+                            Türkiye Hava Durumu
                         </h3>
                     </Col>
 
                 </Row>
 
+
+
                 <Row>
 
                     <Col xs="2" className='my-3'>
-                        <select className="form-select" aria-label="Default select example" value={weatherCitiesFilter.name} onChange={(e) => setCity(e.target.value)}>
+                        <select className="form-select" aria-label="Default select example" value={weatherCitiesFilter.name} onChange={(e) => handleChange(e.target.value)}>
                             <option selected>Şehir seçiniz</option>
+
+
 
 
                             {
                                 weatherCities.map((city, index) => (
 
-                                    <option key={index} value={city.id}>{city.name}</option>
+                                    <option key={index} value={index}>{city.name}</option>
 
                                 ))
                             }
@@ -71,75 +93,149 @@ function Main() {
 
                 <Row className='d-flex justify-content-around'>
 
-                    <Col className='mt-5'>
-                        <div className="card text-center" style={{ width: "10rem" }}>
-                            <h4 className="card-text mt-2">Sun</h4>
-                            <img className="card-img-top rounded mx-auto d-block" src="https://cdn-icons-png.flaticon.com/512/0/74.png" alt="weather" style={{ width: "5rem" }} />
-                            <div className="card-body">
-                                <p className="card-text">78° / 67°</p>
-                            </div>
-                        </div>
-                    </Col>
 
-                    <Col className='mt-5'>
-                        <div className="card text-center" style={{ width: "10rem" }}>
-                            <h4 className="card-text mt-2">Mon</h4>
-                            <img className="card-img-top rounded mx-auto d-block" src="https://cdn-icons-png.flaticon.com/512/0/74.png" alt="weather" style={{ width: "5rem" }} />
-                            <div className="card-body">
-                                <p className="card-text">78° / 67°</p>
-                            </div>
-                        </div>
-                    </Col>
+                    {
+                        weathers ?
 
-                    <Col className='mt-5'>
-                        <div className="card text-center" style={{ width: "10rem" }}>
-                            <h4 className="card-text mt-2">Tue</h4>
-                            <img className="card-img-top rounded mx-auto d-block" src="https://cdn-icons-png.flaticon.com/512/0/74.png" alt="weather" style={{ width: "5rem" }} />
-                            <div className="card-body">
-                                <p className="card-text">78° / 67°</p>
-                            </div>
-                        </div>
-                    </Col>
+                            <Col className='mt-5'>
+                                <div className="card text-center" style={{ width: "10rem" }}>
 
-                    <Col className='mt-5'>
-                        <div className="card text-center" style={{ width: "10rem" }}>
-                            <h4 className="card-text mt-2">Wed</h4>
-                            <img className="card-img-top rounded mx-auto d-block" src="https://cdn-icons-png.flaticon.com/512/0/74.png" alt="weather" style={{ width: "5rem" }} />
-                            <div className="card-body">
-                                <p className="card-text">78° / 67°</p>
-                            </div>
-                        </div>
-                    </Col>
 
-                    <Col className='mt-5'>
-                        <div className="card text-center" style={{ width: "10rem" }}>
-                            <h4 className="card-text mt-2">Thu</h4>
-                            <img className="card-img-top rounded mx-auto d-block" src="https://cdn-icons-png.flaticon.com/512/0/74.png" alt="weather" style={{ width: "5rem" }} />
-                            <div className="card-body">
-                                <p className="card-text">78° / 67°</p>
-                            </div>
-                        </div>
-                    </Col>
+                                    <h4 className="card-text mt-2">{dateMon}</h4>
+                                    <img className="card-img-top rounded mx-auto d-block" src={`http://openweathermap.org/img/wn/${weathers.daily[0].weather[0].icon}@2x.png`} alt="weather" style={{ width: "5rem" }} />
+                                    <div className="card-body">
+                                        <p className="card-text">{weathers.daily[0].temp.max} / {weathers.daily[0].temp.min}</p>
+                                    </div>
+                                </div>
+                            </Col>
 
-                    <Col className='mt-5'>
-                        <div className="card text-center" style={{ width: "10rem" }}>
-                            <h4 className="card-text mt-2">Fri</h4>
-                            <img className="card-img-top rounded mx-auto d-block" src="https://cdn-icons-png.flaticon.com/512/0/74.png" alt="weather" style={{ width: "5rem" }} />
-                            <div className="card-body">
-                                <p className="card-text">78° / 67°</p>
-                            </div>
-                        </div>
-                    </Col>
+                            :
 
-                    <Col className='mt-5'>
-                        <div className="card text-center" style={{ width: "10rem" }}>
-                            <h4 className="card-text mt-2">Sat</h4>
-                            <img className="card-img-top rounded mx-auto d-block" src="https://cdn-icons-png.flaticon.com/512/0/74.png" alt="weather" style={{ width: "5rem" }} />
-                            <div className="card-body">
-                                <p className="card-text">78° / 67°</p>
-                            </div>
-                        </div>
-                    </Col>
+                            null
+                    }
+
+                    {
+                        weathers ?
+
+                            <Col className='mt-5'>
+                                <div className="card text-center" style={{ width: "10rem" }}>
+
+
+                                    <h4 className="card-text mt-2">Salı</h4>
+                                    <img className="card-img-top rounded mx-auto d-block" src={`http://openweathermap.org/img/wn/${weathers.daily[1].weather[0].icon}@2x.png`} alt="weather" style={{ width: "5rem" }} />
+                                    <div className="card-body">
+                                        <p className="card-text">{weathers.daily[1].temp.max} / {weathers.daily[1].temp.min}</p>
+                                    </div>
+                                </div>
+                            </Col>
+
+                            :
+
+                            null
+                    }
+
+                    {
+                        weathers ?
+
+                            <Col className='mt-5'>
+                                <div className="card text-center" style={{ width: "10rem" }}>
+
+
+                                    <h4 className="card-text mt-2">Çarşamba</h4>
+                                    <img className="card-img-top rounded mx-auto d-block" src={`http://openweathermap.org/img/wn/${weathers.daily[2].weather[0].icon}@2x.png`} alt="weather" style={{ width: "5rem" }} />
+                                    <div className="card-body">
+                                        <p className="card-text">{weathers.daily[2].temp.max} / {weathers.daily[2].temp.min}</p>
+                                    </div>
+                                </div>
+                            </Col>
+
+                            :
+
+                            null
+                    }
+
+                    {
+                        weathers ?
+
+                            <Col className='mt-5'>
+                                <div className="card text-center" style={{ width: "10rem" }}>
+
+
+                                    <h4 className="card-text mt-2">Perşembe</h4>
+                                    <img className="card-img-top rounded mx-auto d-block" src={`http://openweathermap.org/img/wn/${weathers.daily[3].weather[0].icon}@2x.png`} alt="weather" style={{ width: "5rem" }} />
+                                    <div className="card-body">
+                                        <p className="card-text">{weathers.daily[3].temp.max} / {weathers.daily[3].temp.min}</p>
+                                    </div>
+                                </div>
+                            </Col>
+
+                            :
+
+                            null
+                    }
+
+                    {
+                        weathers ?
+
+                            <Col className='mt-5'>
+                                <div className="card text-center" style={{ width: "10rem" }}>
+
+
+                                    <h4 className="card-text mt-2">Cuma</h4>
+                                    <img className="card-img-top rounded mx-auto d-block" src={`http://openweathermap.org/img/wn/${weathers.daily[4].weather[0].icon}@2x.png`} alt="weather" style={{ width: "5rem" }} />
+                                    <div className="card-body">
+                                        <p className="card-text">{weathers.daily[4].temp.max} / {weathers.daily[4].temp.min}</p>
+                                    </div>
+                                </div>
+                            </Col>
+
+                            :
+
+                            null
+                    }
+
+                    {
+                        weathers ?
+
+                            <Col className='mt-5'>
+                                <div className="card text-center" style={{ width: "10rem" }}>
+
+
+                                    <h4 className="card-text mt-2">Cumartesi</h4>
+                                    <img className="card-img-top rounded mx-auto d-block" src={`http://openweathermap.org/img/wn/${weathers.daily[5].weather[0].icon}@2x.png`} alt="weather" style={{ width: "5rem" }} />
+                                    <div className="card-body">
+                                        <p className="card-text">{weathers.daily[5].temp.max} / {weathers.daily[5].temp.min}</p>
+                                    </div>
+                                </div>
+                            </Col>
+
+                            :
+
+                            null
+                    }
+
+                    {
+                        weathers ?
+
+                            <Col className='mt-5'>
+                                <div className="card text-center" style={{ width: "10rem" }}>
+
+
+                                    <h4 className="card-text mt-2">Pazar</h4>
+                                    <img className="card-img-top rounded mx-auto d-block" src={`http://openweathermap.org/img/wn/${weathers.daily[6].weather[0].icon}@2x.png`} alt="weather" style={{ width: "5rem" }} />
+                                    <div className="card-body">
+                                        <p className="card-text">{weathers.daily[6].temp.max} / {weathers.daily[6].temp.min}</p>
+
+                                    </div>
+                                </div>
+                            </Col>
+
+                            :
+
+                            null
+                    }
+
+
 
 
 
@@ -154,3 +250,5 @@ function Main() {
 }
 
 export default Main
+
+
